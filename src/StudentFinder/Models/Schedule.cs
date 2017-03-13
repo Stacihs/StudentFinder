3cc745d7c5976543931364e16b39e461c316e2f6
RummenacherK <Krummenacher@live.com>
2017-03-13 14:12:36 -0500

    Using DateTime in Schedules DB

--- src/StudentFinder/Models/Schedule.cs
+++ src/StudentFinder/Models/Schedule.cs
@@ -13,10 +13,39 @@ namespace StudentFinder.Models
         public int Id { get; set; }
         [Required]
         public string Label { get; set; }
+        
+        public DateTime? From { get; set; }
+       
         [Required]
-        public string From { get; set; }
+        [RegularExpression(@"^([1-9]|0[1-9]|1[0-2]):[0-5][0-9](am|pm|AM|PM)$", ErrorMessage = "Invalid Time.")]
+        public string FromValue
+        {
+            get
+            {
+                return From.HasValue ? From.Value.ToString("hh:mm tt") : string.Empty;
+            }
+
+            set
+            {
+                From = DateTime.Parse(value);
+            }
+        }
+        
+        public DateTime? To { get; set; }
         [Required]
-        public string To { get; set; }
+        [RegularExpression(@"^([1-9]|0[1-9]|1[0-2]):[0-5][0-9](am|pm|AM|PM)$", ErrorMessage = "Invalid Time.")]
+        public string ToValue
+        {
+            get
+            {
+                return To.HasValue ? To.Value.ToString("hh:mm tt") : string.Empty;
+            }
+
+            set
+            {
+                To = DateTime.Parse(value);
+            }
+        }
 
         public ICollection<StudentScheduleSpace> StudentScheduleSpace { get; set; }
         //public virtual Schedule Schedules { get; set; }
