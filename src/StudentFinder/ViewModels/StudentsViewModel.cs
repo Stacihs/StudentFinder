5c606b08b93ee5b200490b2a430ce83e6ca2102e
Joel Gilbert <gilbert.279@gmail.com>
2017-03-12 21:11:29 -0500

    Added Pagination to Student Index

--- src/StudentFinder/ViewModels/StudentsViewModel.cs
+++ src/StudentFinder/ViewModels/StudentsViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace StudentFinder.Models.ViewModels
+namespace StudentFinder.ViewModels
 {
     public class StudentsViewModel
     {
